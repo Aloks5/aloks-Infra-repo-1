42c18813efeea67551394310981bc41e666ba59d
Aloks05 <alokawscloud16@gmail.com>
2023-06-14 00:47:37 +0530

    updated files 15

--- backend.tf
+++ backend.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "my-aws-tf-state-bucket-updated"
     key = "terraform.tfstate"
     region = "eu-central-1"
-    dynamodb_table = "new-dynamo-db-table"
+    dynamodb_table = "my-dynamo-db-table
+"
   }
 }
