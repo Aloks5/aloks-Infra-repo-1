47cbd7162a87ee85f102e9dc15b98443ebda3390
Aloks05 <alokawscloud16@gmail.com>
2023-06-14 13:06:15 +0530

    updated files 20

--- s3.tf
+++ s3.tf
@@ -1,11 +1,16 @@
-resource "aws_s3_bucket" "my-new-s3-bucket" {
-  bucket_prefix = var.bucket_prefix
-  acl = var.acl
-  force_destroy = true
+# resource "aws_s3_bucket" "my-new-s3-bucket" {
+#   bucket_prefix = var.bucket_prefix
+#   acl = var.acl
+#   force_destroy = true
+
+#    versioning {
+#     enabled = var.versioning
+#   }
   
-   versioning {
-    enabled = var.versioning
-  }
-  
-  tags = var.tags
+#   tags = var.tags
+# }
+
+resource "aws_s3_bucket" "spacelift-test1-s3" {
+    bucket = "aloks-new-jenkins-cicd-test1" 
+    acl = "private"   
 }
